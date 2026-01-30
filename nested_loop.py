8f68764cefc0c4f70e9b0dabacd19ce006d02aed
mukunda001 <mukunda8702@gmail.com>
2026-01-30 21:39:27 +0545

    Nested Loop Examples

---
+++ nested_loop.py
@@ -0,0 +1,64 @@
+
+# for i in range(3):
+#     for j in range(2):
+#         print(i, j)
+
+#######################################
+
+# matrix = [[1,2],[3,4]]
+
+# for row in matrix:
+#     for val in row:
+#         print(val, end=' ')
+#     print()         # for new line after each row
+
+####################################################
+
+# char = '*'
+# for _ in range(4):
+#     for _ in range(4):
+#         print(char, end = ' ')
+#     print()
+#######################################################   
+
+# val = 1
+# for i in range(3):
+#     for _ in range(3):
+#         print(val, end = ' ')
+#         val += 1
+#     print()
+#################################################
+
+# Finding duplicate in the list
+# nums = [1,2,2,2,3,4,5,5]
+# dups = []
+# for i in range(len(nums)):
+#     for j in range(i+1,len(nums)):
+#         if nums[i] == nums[j] and nums[i] not in dups:
+#            dups.append(nums[i])
+
+# print(dups)
+
+#######################################################
+
+#Right Angle Triangle Pattern printing
+# rows = 8
+# for i in range(1,rows+1):
+#     for _ in range(rows - i):
+#         print(" ", end = "")
+#     for j in range(i):
+#         print("*", end = "")
+#     print()
+############## ALTERNATIVE #################
+
+        # for i in range(1,6):
+        #     print(" "*(5-i)+"*"*i)
+
+#######################################################
+
+
+    
+
+   
+
+
